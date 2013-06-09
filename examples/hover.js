c340f9c00b275de10772e2a36ff736c0871fcec2
C J Silverio <ceejceej@gmail.com>
2013-06-09 16:06:16 -0700

    IT'S ALIVE!

--- examples/hover.js
+++ examples/hover.js
@@ -3,9 +3,28 @@ var
 	;
 
 
-var copter = new Aerogel.Copter();
 
-copter.connect('radio://1/2/2MPS')
+var driver = new Aerogel.CrazyDriver();
+var copter = new Aerogel.Copter(driver);
+process.on('SIGINT', copter.land.bind(copter));
+
+driver.findCopters()
+.then(function(copters)
+{
+	if (copters.length === 0)
+	{
+		console.error('No copters found! Is your copter turned on?');
+		process.exit(1);
+	}
+
+	var uri = copters[0];
+	console.log('Using copter at', uri);
+	return uri;
+})
+.then(function(uri)
+{
+	return copter.connect(uri);
+})
 .then(function()
 {
 	return copter.takeoff();
@@ -21,10 +40,11 @@ copter.connect('radio://1/2/2MPS')
 			console.log(response);
 			process.exit(0);
 		});
-	}, 5000);
+	}, 1000);
 })
 .fail(function(err)
 {
+	console.log(err);
 	copter.shutdown()
 	.then(function(response)
 	{
