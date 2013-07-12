c3e04a692e81cb554ead8064e74901ca9a12df04
C J Silverio <ceejceej@gmail.com>
2013-07-12 08:49:54 -0700

    Cleaned up the parameter/telemetry logging script thanks to the promise change.

--- examples/telemetry.js
+++ examples/telemetry.js
@@ -2,17 +2,15 @@ var Aerogel = require('../index');
 
 var driver = new Aerogel.CrazyDriver();
 var copter = new Aerogel.Copter(driver);
+var params;
 process.on('SIGINT', bail);
 
 console.log('just connecting & sitting to log some telemetry info...');
 
 function bail()
 {
-	return copter.land()
-	.then(function()
-	{
-		return copter.shutdown();
-	})
+	console.log('entered bail()');
+	return copter.shutdown()
 	.then(function()
 	{
 		return process.exit(0);
@@ -26,44 +24,7 @@ function bail()
 	.done();
 }
 
-
-copter.on('ready', function()
-{
-	console.log('got all telemetry & parameters');
-
-	var params = copter.driver.parameters.all();
-	// console.log(params);
-
-	copter.driver.parameters.get('cpu.id0')
-	.then(function(value)
-	{
-		console.log('cpu.id0 ==', value);
-		shutdown();
-	});
-});
-
-function shutdown()
-{
-	copter.shutdown()
-	.then(function(response)
-	{
-		console.log(response);
-		process.exit(0);
-	})
-	.fail(function(err)
-	{
-		console.log(err);
-		copter.shutdown()
-		.then(function(response)
-		{
-			console.log(response);
-			process.exit(1);
-		});
-	})
-	.done();
-}
-
-setTimeout(shutdown, 30000);
+setTimeout(bail, 30000);
 
 driver.findCopters()
 .then(function(copters)
@@ -81,5 +42,33 @@ driver.findCopters()
 .then(function(uri)
 {
 	return copter.connect(uri);
+})
+.then(function()
+{
+	console.log('got all telemetry & parameters');
+
+	params = copter.driver.parameters.all();
+	console.log(params);
+	return copter.driver.parameters.get('pid_attitude.yaw_kd');
+})
+.then(function(value)
+{
+	console.log('pid_attitude.yaw_kd:', value);
+	return copter.shutdown();
+})
+.then(function(response)
+{
+	console.log('Shutdown complete.');
+	process.exit(0);
+})
+.fail(function(err)
+{
+	console.log(err);
+	copter.shutdown()
+	.then(function(response)
+	{
+		console.log(response);
+		process.exit(1);
+	});
 })
 .done();
